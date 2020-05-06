255b8a73c471e9140a08d2621d87250302e0173c
Jaki <chimuud_kh@yahoo.com>
2020-05-06 16:49:09 -0700

    Removed uSQLUtils

--- uMembers.pas
+++ uMembers.pas
@@ -2,80 +2,152 @@ unit uMembers;
 
 interface
 
+uses
+  Web.HTTPApp, System.SysUtils, System.Classes, RTTI, ADODB,
+  uDatabase; //located in Database-Access
+
 type
-  TMembers = class
+  TMembers = class(TADOQuery)
   private
-    FMemberId: Integer;
-    FFirstName: string;
-    FMiddleName: string;
+    FADOConnection: TSQLDatabase;
+
+    FId: Integer;
     FLastName: string;
+    FMiddleName: string;
+    FFirstName: string;
     FAge: Integer;
-    function GetAge: Integer;
+
+    function GetId: Integer;
     function GetFirstName: string;
     function GetLastName: string;
     function GetMiddleName: string;
-    function GetMemberId: Integer;
+    function GetAge: Integer;
+
+    function ToJSON: string;
   public
-    property MemberId: Integer read GetMemberId write FMemberId;
+    property Id: Integer read GetId write FId;
     property FirstName: string read GetFirstName write FFirstName;
     property MiddleName: string read GetMiddleName write FMiddleName;
     property LastName: string read GetLastName write FLastName;
     property Age: Integer read GetAge write FAge;
 
-    function GetMember(Id: Integer): TMembers;
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    procedure Open(SQLText: string); overload;
+    function GetMember(Id: Integer): string;
+    procedure SavePicture(Id: Integer; Stream: TMemoryStream);
+  published
+
   end;
 
 implementation
 
-{ TMembers }
+uses
+  JSON, Data.DB;
 
-// *** Properties ***
+{ TFamily }
 
-function TMembers.GetMemberId: Integer;
+constructor TMembers.Create(AOwner: TComponent);
 begin
-  Result := FMemberId;
+  inherited;
+//  FADOConnection := TSQLDatabase.Create(nil);
+//  FADOConnection.ConnectDB('Family');
+  Connection := TSQLDatabase.ConnectDB('Family');
 end;
 
-function TMembers.GetAge: Integer;
+destructor TMembers.Destroy;
 begin
-  Result := FAge;
+  FADOConnection.Free;
+  inherited;
+end;
+
+{ *** Properties ***}
+
+function TMembers.GetAge: Integer;
+var
+  Field: TField;
+begin
+  Result := 0;
+  Field := FieldByName('Age');
+  if not Field.IsNull then
+    Result := FieldByName('Age').AsInteger;
 end;
 
 function TMembers.GetFirstName: string;
 begin
-  Result := FFirstName;
+  Result := FieldByName('FirstName').AsString;
+end;
+
+function TMembers.GetId: Integer;
+begin
+  Result := FieldByName('Id').AsInteger;
 end;
 
 function TMembers.GetLastName: string;
 begin
-  Result := FLastName;
+  Result := FieldByName('LastName').AsString;
 end;
 
 function TMembers.GetMiddleName: string;
 begin
-  Result := FMiddleName;
+  Result := FieldByName('MiddleName').AsString;
 end;
 
-// *** End Properties ***
+{ *** End Properties ***}
 
-function TMembers.GetMember(Id: Integer): TMembers;
+procedure TMembers.Open(SQLText: string);
 begin
-  if Id = 0 then
+  SQL.Text := SQLText;
+  inherited Open;
+end;
+
+procedure TMembers.SavePicture(Id: Integer; Stream: TMemoryStream);
+var
+  qry: TADOQuery;
+  cnt: Integer;
+begin
+  Stream.Position := 0;
+  qry := TADOQuery.Create(nil);
+  try
+    qry.Connection := TSQLDatabase.ConnectDB('Family');
+    qry.SQL.Text :=
+      'UPDATE Members '+
+      'SET Picture = :Picture '+
+      'WHERE Id = ' + Id.ToString;
+    qry.Parameters.ParamByName('Picture').LoadFromStream(Stream, ftBlob);
+    cnt := qry.ExecSQL;
+  finally
+    qry.Free;
+  end;
+end;
+
+function TMembers.ToJSON: string;
+var
+  o: TJSONObject;
+begin
+  try
+    o := TJSONObject.Create;
+    o.AddPair('id', TJSONNumber.Create(Id));
+    o.AddPair('FirstName', FirstName);
+    o.AddPair('LastName', LastName);
+    o.AddPair('Age', TJSONNumber.Create(Age));
+    Result := o.Format(2);
+  finally
+    o.Free;
+  end;
+end;
+
+function TMembers.GetMember(Id: Integer): string;
+var
+  sql: string;
+begin
+  Result := '';
+  if not Assigned(self) then Exit;
+
+   if Locate('Id', Id, []) then
   begin
-    FirstName := 'Jon';
-    LastName := 'Don';
-    Age := 35;
-  end else
-  if Id = 1 then
-  begin
-    FirstName := 'Bob';
-    LastName := 'Eaton';
-    Age := 44;
-  end else
-  begin
-    FirstName := '';
-    LastName := '';
-    Age := 0;
+    Result := ToJSON;
   end;
 end;
 
